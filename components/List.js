7a80595d79e54705527df42121d9384c785bea7a
Merijn Vonk <merijnvonk@gmail.com>
2022-07-19 11:32:28 +0200

    commit for feedback

---
+++ components/List.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+
+class List extends Component() {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        <div>
+            <ul>
+                <li>Kwark</li>
+                <li>Eieren</li>
+                <li>Beleg</li>
+                <li>Fruit</li>
+                <li>Brood</li>
+                <li>Snacks</li>
+                <li>Breakers</li>
+            </ul>
+        </div>
+    }
+}
+
+
+
+export default List
